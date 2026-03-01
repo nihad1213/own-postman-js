77d4f011ba66ccedef4eed01e3e70742195a36da
nihad1213 <nihad.nemetli@gmail.com>
2026-03-01 13:46:51 +0400

    new: add button remove button functionalties created

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+const queryParamsContainer = document.querySelector('[data-add-query-params]')
+const requestHeadersContainer = document.querySelector('[data-add-request-headers]')
+const keyValueTemplate = document.querySelector('[data-key-value-template]')
+
+document.querySelector('[data-add-query-param-btn]')
+  .addEventListener('click', () => {
+      queryParamsContainer.append(createKeyValuePair())
+})
+
+document.querySelector('[data-add-request-header-btn]')
+  .addEventListener('click', () => {
+      requestHeadersContainer.append(createKeyValuePair())
+})
+
+queryParamsContainer.append(createKeyValuePair())
+requestHeadersContainer.append(createKeyValuePair())
+
+function createKeyValuePair() {
+    const element = keyValueTemplate.content.cloneNode(true)
+    element.querySelector('[data-remove-btn]')
+      .addEventListener('click', (e) => {
+          e.target.closest('[data-key-value-pair]').remove()
+      })
+    return element
+}
